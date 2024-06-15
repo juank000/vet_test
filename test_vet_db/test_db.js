c8a55146401c97162a1527107a70321e9c38c734
juanc <juanc@email.com>
2024-06-15 11:15:52 -0700

    vet project files

---
+++ test_vet_db/test_db.js
@@ -0,0 +1,50 @@
+export const USERS_DB = [
+  {
+    "id": "964c2ceb-eee4-41ca-b223-82f7d4855098",
+    "name": "Muriel",
+    "last_name": "Buckley",
+    "birthdate": "2023-12-23T10:53:44 +08:00"
+  },
+  {
+    "id": "68794a6e-3767-4675-8d03-9ae7672af49e",
+    "name": "Clements",
+    "last_name": "Holt",
+    "birthdate": "2015-03-03T04:04:29 +08:00"
+  },
+  {
+    "id": "32c4c7ba-2cf9-44ae-8aea-60e1d5ec277d",
+    "name": "Greer",
+    "last_name": "Guy",
+    "birthdate": "2020-10-12T08:12:26 +07:00"
+  },
+  {
+    "id": "7c94a04d-39ce-4085-a863-536bf726117c",
+    "name": "Paulette",
+    "last_name": "Mclaughlin",
+    "birthdate": "2022-07-12T07:37:06 +07:00"
+  },
+  {
+    "id": "466205a6-bdf1-49e2-902b-56c6d0f3a9f8",
+    "name": "Calhoun",
+    "last_name": "Parks",
+    "birthdate": "2015-01-24T11:02:51 +08:00"
+  },
+  {
+    "id": "f3e64aad-e3cf-4529-8b9e-bc1591b4278d",
+    "name": "Bates",
+    "last_name": "Winters",
+    "birthdate": "2024-06-07T09:48:03 +07:00"
+  },
+  {
+    "id": "58897a01-1e84-42cb-ba43-27a0f370da6d",
+    "name": "Augusta",
+    "last_name": "Johnson",
+    "birthdate": "2018-01-25T08:23:39 +08:00"
+  },
+  {
+    "id": "72d5e464-515d-4a8b-9299-69102fdc518d",
+    "name": "Lela",
+    "last_name": "Ball",
+    "birthdate": "2023-07-14T09:34:52 +07:00"
+  }
+]
